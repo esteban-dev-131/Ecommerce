00f9887fe27d7fd604d08d89665df348e6476f08
esteban-dev-131 <camellovilla25@gmail.com>
2026-04-12 16:42:00 -0500

    feat: Design added to Login.js and App.js
    Design has been applied to the Login and App files. In Login, a panel was added, along with a text field, a sign-in button, and an image. In App.js, the header was improved with a background color and more formal text.

--- App.js
+++ App.js
@@ -2,19 +2,55 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // import StackNavigator
 import 'react-native-gesture-handler';
+// import navigation container
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+// import screens
+import Login from './screens/Login';
+import Home from './screens/Home';
 
-
-
-/*Componente cuerpo*/
+/*Main Component*/
 export default function App() {
+
+const Stack = createStackNavigator();
+
+//Stack Navigator
+function MyStack() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login}
+      // Opciones para el header de la pantalla de login
+      options={{
+        title:'LOGIN',
+        headerTintColor: 'white',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#525FE1',
+        },
+      }}/>
+      <Stack.Screen name="Home" component={Home}
+      //Lo mismo para la pantalla de home
+      options={{
+        title:'HOME',
+        headerTintColor: 'white',
+        headerTitleAlign: 'center',
+        headerStyle: {
+          backgroundColor: '#525FE1',
+        },
+      }} />
+    </Stack.Navigator>
   );
 }
-/*Estilos*/
+
+
+
+  return (
+  <NavigationContainer>
+    <MyStack/>
+  </NavigationContainer>
+  );
+}
+/*Styles*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,102 @@
 //se pone "rcns" para crear un componente de clase
 import React, { useState } from "react";
-import { Text, StyleSheet, View } from "react-native";
+import {
+  Text,
+  StyleSheet,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
 
 // Se crea un componente de clase llamado Login y se pone como funcion
 export default function Login() {
-  // se elimina el render
   return (
-    <View>
-      <Text> Login </Text>
+    // Se crea una vista padre para centrar el contenido
+    <View style={styles.padre}>
+      // Se crea una vista para la imagen de perfil
+      <View>
+        <Image
+          source={require("../assets/profile.jpg")}
+          style={styles.profile}
+        />
+      </View>
+      <View style={styles.tarjeta}>
+        //Email
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder="Email" style={{ paddingHorizontal: 15 }} />
+        </View>
+        //Contraseña
+        <View style={styles.cajaTexto}>
+          <TextInput placeholder="Password" style={{ paddingHorizontal: 15 }} />
+        </View>
+        // Boton de inicio de sesion
+        <View style={styles.PadreBoton}>
+          <TouchableOpacity style = {styles.CajaBoton}  >
+            <Text style={styles.TextoBoton}>Sign In</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  padre: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  // Estilo para la imagen de perfil
+  profile: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    borderColor: "white",
+  },
+  //tarjeta que contiene el formulario de login, con sombra y borde redondeado
+  tarjeta: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    width: "90%",
+    padding: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  //Estilo caja de Texto (TextInput)
+  cajaTexto:{
+    paddingVertical: 20,
+    backgroundColor: '#cccccc40',
+    borderRadius: 30,
+    marginVertical: 10,
+  },
+  // Estilo para el boton de inicio de sesion
+  PadreBoton:{
+    alignItems:'center'
+  },
+  // Estilo para el texto del boton de inicio de sesion
+  CajaBoton:{
+    backgroundColor:'#525FE1',
+    borderRadius:30,
+    paddingVertical:20,
+    paddingHorizontal:40,
+    width:150,
+    marginTop:20,
+  },
+  // Estilo para el texto del boton de inicio de sesion
+  TextoBoton:{
+    textAlign:'center',
+    color:'white',
+    fontWeight:'bold',
+  }
+
+
+});
